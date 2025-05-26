0aa0b620756be86a6cdfb4b2a90a1a373761c959
BlueRabbit-kim <whddbs1594@gmail.com>
2025-05-26 09:41:36 +0900

    complet

---
+++ src/LMS/StudentUser.java
@@ -0,0 +1,73 @@
+package LMS;
+public class StudentUser {
+    private String sno;
+    private String snm;
+    private String sid;
+    private String spw;
+    private String sdate;
+
+    public StudentUser(String sno, String snm) {
+        this.sno = sno;
+        this.snm = snm;
+        this.sid = sid;
+        this.spw = spw;
+        this.sdate = sdate;
+    }
+
+//    public String getMid() {
+//        return mid;
+//    }
+//
+//    public void setMid(String mid) {
+//        this.mid = mid;
+//    }
+//
+//    public String getMpw() {
+//        return mpw;
+//    }
+//
+//    public void setMpw(String mpw) {
+//        this.mpw = mpw;
+//    }
+//
+//    public String getMnm() {
+//        return mnm;
+//    }
+//
+//    public void setMnm(String mnm) {
+//        this.mnm = mnm;
+//    }
+//
+//    public String getMph() {
+//        return mph;
+//    }
+//
+//    public void setMph(String mph) {
+//        this.mph = mph;
+//    }
+//
+//    public String getMzip() {
+//        return mzip;
+//    }
+//
+//    public void setMzip(String mzip) {
+//        this.mzip = mzip;
+//    }
+//
+//    public String getMaddr() {
+//        return maddr;
+//    }
+
+
+//    @Override
+//    public String toString() {
+//        return "회원 정보{" +
+//                "아이디 ='" + sid + '\'' +
+//                ", 비밀번호 ='" + mpw + '\'' +
+//                ", 이름 ='" + mnm + '\'' +
+//                ", 연락처 ='" + mph + '\'' +
+//                ", 우편번호 ='" + mzip + '\'' +
+//                ", 주소 ='" + maddr + '\'' +
+//                '}';
+//    }
+}
